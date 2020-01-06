628639adfe1d384c8694eba6459236713851a80b
mantou132 <709922234@qq.com>
2020-01-06 19:26:17 +0800

    Use API fetch lyrics

--- src/content/lyrics.ts
+++ src/content/lyrics.ts
@@ -1,28 +1,33 @@
-const lyricExample = `
-[ti:海阔天空]
-[ar:Beyond]
-[al:]
- 
-[00:00]Beyond：海阔天空
-[01:40][00:07]今天我寒夜里看雪飘过
-[01:48][00:24]怀着冷却了的心窝飘远方
-[01:53][00:29]风雨里追赶
-[01:57][00:33]雾里分不清影踪
-[02:00][00:36]天空海阔你与我
-[02:03][00:39]可会变(谁没在变)
-[00:42]多少次迎着冷眼与嘲笑
-[00:49]从没有放弃过心中的理想
-[00:54]一刹那恍惚
-[00:58]若有所失的感觉
-[01:01]不知不觉已变淡
-[01:04]心里爱(谁明白我)
-[03:56][03:13][02:06][01:07]原谅我这一生不羁放纵爱自由
-[04:01][03:24][02:12][01:13]也会怕有一天会跌倒
-[04:06][03:44][03:29][02:19][01:20]被弃了理想谁人都可以
-[04:14][03:49][03:37][02:26][01:26]那会怕有一天只你共我
-[03:02]仍然自由自我
-[03:10]永远高唱我歌
-`;
+/* eslint-disable @typescript-eslint/camelcase */
+import { Query } from './song';
+
+interface SearchResult {
+  result?: {
+    songs?: { id?: number }[];
+  };
+}
+
+interface SongResult {
+  lrc?: {
+    lyric?: string;
+  };
+}
+
+const API = 'https://api.imjad.cn/cloudmusic/';
+async function fetchLyric(s: string) {
+  try {
+    const { result }: SearchResult = await (
+      await fetch(`${API}?${new URLSearchParams({ type: 'search', search_type: '1', s })}`)
+    ).json();
+    const songId = result?.songs?.[0]?.id;
+    const { lrc }: SongResult = await (
+      await fetch(`${API}?${new URLSearchParams({ type: 'lyric', id: String(songId) })}`)
+    ).json();
+    return lrc?.lyric || '';
+  } catch {
+    return '';
+  }
+}
 
 class Line {
   startTime: number | null = null;
@@ -32,8 +37,10 @@ class Line {
 export type Lyric = Line[];
 
 export let lyric: Lyric = [];
-export function updateLyric() {
-  const lyricStr = lyricExample;
+export async function updateLyric({ name, artist }: Query) {
+  lyric = [];
+  const lyricStr = await fetchLyric(`${name}-${artist}`);
+  if (!lyricStr) return;
   const lines = lyricStr.split('\n').map(line => line.trim());
   lyric = lines
     .map(line => {
--- src/content/song.ts
+++ src/content/song.ts
@@ -1,8 +1,21 @@
 const TRACK_INFO_SELECTOR = '.track-info';
+const TRACK_NAME_SELECTOR = '.track-info__name';
+const TRACK_ARTIST_SELECTOR = '.track-info__artists';
+
+export interface Query {
+  name: string;
+  artist: string;
+}
+
+function getQueryObj(element: Element): Query {
+  const name = element.querySelector(TRACK_NAME_SELECTOR)?.textContent;
+  const artist = element.querySelector(TRACK_ARTIST_SELECTOR)?.textContent;
+  return { name: name || '', artist: artist || '' };
+}
 
 const weakMap = new WeakMap<Element, MutationObserver>();
 
-export default function songObserver(callback: Function) {
+export default function songObserver(callback: (query: Query) => any) {
   let trackInfo: Element | null = null;
   const observer = new MutationObserver(() => {
     const element = document.querySelector(TRACK_INFO_SELECTOR);
@@ -12,9 +25,9 @@ export default function songObserver(callback: Function) {
         ob?.disconnect();
       }
     } else if (!weakMap.has(element)) {
-      callback();
+      callback(getQueryObj(element));
       const observer = new MutationObserver(() => {
-        callback();
+        callback(getQueryObj(element));
       });
       observer.observe(element, { childList: true, characterData: true, subtree: true, attributes: true });
       weakMap.set(element, observer);
