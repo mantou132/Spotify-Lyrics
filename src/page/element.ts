c2e7571b39128611554e8483db242d35db13be69
mantou <709922234@qq.com>
2020-04-16 19:37:43 +0800

    Fixed #20

--- src/page/element.ts
+++ src/page/element.ts
@@ -5,10 +5,11 @@ let audio: HTMLAudioElement | null = null;
 document.createElement = function<K extends keyof HTMLElementTagNameMap>(tagName: K, options: ElementCreationOptions) {
   const element = createElement(tagName, options);
   if (tagName === 'video') {
-    video = element as HTMLVideoElement;
-  }
-  if (tagName === 'audio') {
-    audio = element as HTMLAudioElement;
+    if (!audio) {
+      audio = element as HTMLAudioElement;
+    } else {
+      video = element as HTMLVideoElement;
+    }
   }
   return element;
 };
