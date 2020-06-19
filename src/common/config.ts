184b3ce431245375309e1a0f4f3b892747c0f986
mantou132 <709922234@qq.com>
2020-06-19 13:11:47 +0800

    Clean up code

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,27 +1,16 @@
 import config from './config.json';
 
-// fallback
-class Config {
-  API_HOST = config.API_HOST;
-  LYRICS_CONTAINER_SELECTOR = config.LYRICS_CONTAINER_SELECTOR;
-  ALBUM_COVER_SELECTOR = config.ALBUM_COVER_SELECTOR;
-  TRACK_INFO_SELECTOR = config.TRACK_INFO_SELECTOR;
-  TRACK_NAME_SELECTOR = config.TRACK_NAME_SELECTOR;
-  TRACK_ARTIST_SELECTOR = config.TRACK_ARTIST_SELECTOR;
-  BTN_WRAPPER_SELECTOR = config.BTN_WRAPPER_SELECTOR;
-}
-
 async function getConfig() {
   if (process.env.NODE_ENV === 'production') {
     try {
       return (await fetch(
         'https://raw.githubusercontent.com/mantou132/Spotify-Lyrics/master/src/common/config.json',
-      ).then(res => res.json())) as Config;
+      ).then(res => res.json())) as typeof config;
     } catch {
-      return new Config();
+      return config;
     }
   } else {
-    return new Config();
+    return config;
   }
 }
 
