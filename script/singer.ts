084dcb76f77a0988a21a9436c44090240c79ad06
mantou132 <709922234@qq.com>
2020-07-23 14:40:41 +0800

    Korean song lyrics matching optimization

--- script/singer.ts
+++ script/singer.ts
@@ -13,23 +13,30 @@ interface TopArtistsResult {
   };
 }
 
+type Area = 'c' | 'j' | 'k';
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 const config = JSON.parse(Deno.readTextFileSync('./src/page/config.json'));
 
-async function fetchChineseSingerWithoutAlias(offset: number, toplist = false) {
+async function fetchChineseSingerWithoutAlias(offset: number, toplist: boolean, area: Area) {
+  const map = {
+    c: { type: 1, area: 7 },
+    j: { type: 4, area: 8 },
+    k: { type: 3, area: 16 },
+  };
   let artists: Artist[];
   if (toplist) {
     // https://binaryify.github.io/NeteaseCloudMusicApi/#/?id=%e6%ad%8c%e6%89%8b%e6%a6%9c
-    // type: 1 华语 4 日本
-    const result: TopArtistsResult = await fetch(`${config.API_HOST}/toplist/artist?type=${1}`).then(res => res.json());
+    // type: 1 华语 4 日本 3 韩国
+    const url = `${config.API_HOST}/toplist/artist?type=${map[area].type}`;
+    const result: TopArtistsResult = await fetch(url).then(res => res.json());
     artists = result.list.artists;
   } else {
     // https://binaryify.github.io/NeteaseCloudMusicApi/#/?id=%e6%ad%8c%e6%89%8b%e5%88%86%e7%b1%bb%e5%88%97%e8%a1%a8
-    // area: 7 华语 8 日本
-    const result: ArtistsResult = await fetch(
-      `${config.API_HOST}/artist/list?type=1&area=${7}&offset=${offset}`,
-    ).then(res => res.json());
+    // area: 7 华语 8 日本 16 韩国
+    const url = `${config.API_HOST}/artist/list?type=1&offset=${offset}&area=${map[area].area}`;
+    const result: ArtistsResult = await fetch(url).then(res => res.json());
     artists = result.artists;
   }
   return artists.filter(artist => !artist.alias.length);
@@ -44,15 +51,14 @@ interface SpotifySearchResult {
 }
 // https://developer.spotify.com/console/get-search-item/?q=tania%20bowra&type=artist&market=&limit=&offset=
 async function fetchEnglishNameBySpotify(name: string) {
-  const { artists, error }: SpotifySearchResult = await fetch(
-    `https://api.spotify.com/v1/search?${new URLSearchParams({ q: name, type: 'artist' })}`,
-    {
-      headers: {
-        Authorization:
-          'Bearer BQCI0DUAgop81GGwQziST4wnDPtjUIosVTItNyopkLWhF2h2i36wWLZU8-ySM92gAuDdLX5oeGymz1up_wgc_0oJn2oG7ez-ZjP9Fgkg3L7Ov7mcRN_kzdMkZeti7uxetsPgzy7BBopbGKjFymBzQxEkx2Zn_Hc',
-      },
+  const url = `https://api.spotify.com/v1/search?${new URLSearchParams({ q: name, type: 'artist' })}`;
+  const authorization =
+    'Bearer BQDjsnrlWs_WyvBKf4gQLQyht2MEigW-HtuBXihaC3FBUKdQduC8wTEjf7PbNJiyGWjZp8q12FH41YXNCT9EJn1ejnFFJPl9944oxxxijAbdgLOQdBRFqNGR3ymtrQQXxUUD37_3mpNkJO0-_mX1iWkzlxtmhAM';
+  const { artists, error }: SpotifySearchResult = await fetch(url, {
+    headers: {
+      authorization,
     },
-  ).then(res => res.json());
+  }).then(res => res.json());
   if (error) throw new Error(JSON.stringify(error));
   const enName = artists?.items[0]?.name?.toLowerCase() || '';
   const charCodeTotal = (s: string) => {
@@ -66,10 +72,11 @@ async function fetchEnglishNameBySpotify(name: string) {
   return enName === name ? '' : enName;
 }
 
-async function main(full = true) {
+async function main({ area, test }: { area: Area; test: boolean }) {
   const enZhMap: Record<string, string> = {};
-  const process = async (list: Artist[]) =>
-    await Promise.all(
+  const process = async (list: Artist[]) => {
+    console.log('Top artists:', list.map(e => e.name).join());
+    return await Promise.all(
       list.map(async artist => {
         const enName = await fetchEnglishNameBySpotify(artist.name.toLowerCase());
         if (enName) {
@@ -80,24 +87,24 @@ async function main(full = true) {
         }
       }),
     );
+  };
 
-  await process(await fetchChineseSingerWithoutAlias(0, true));
+  await process(await fetchChineseSingerWithoutAlias(0, true, area));
 
-  if (full) {
-    for (let i = 0; i < 10; i++) {
-      let error = false;
-      const list = await fetchChineseSingerWithoutAlias(i * 100);
-      console.log(list.map(e => e.name).join());
-      try {
-        await process(list);
-      } catch {
-        error = true;
-        console.log('Next loop start:', i);
-      }
-      if (error) break;
+  for (let i = 0; i < (test ? 1 : 10); i++) {
+    let error = false;
+    const list = await fetchChineseSingerWithoutAlias(i * 100, false, area);
+    console.log(`Page ${i + 1} artist list:`, list.map(e => e.name).join());
+    try {
+      await process(list);
+    } catch {
+      error = true;
+      console.log('Next loop start:', i);
     }
+    if (error) break;
   }
   console.log(JSON.stringify(enZhMap, null, 2));
 }
 
-main(false);
+// Due to many network requests, manually modify the parameters to execute
+main({ area: 'c', test: true });
