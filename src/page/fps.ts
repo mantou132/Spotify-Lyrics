2230efbbe6619c604e32731f1c728d93067425c0
mantou132 <709922234@qq.com>
2020-08-20 11:48:06 +0800

    Fixed fps

--- src/page/fps.ts
+++ src/page/fps.ts
@@ -1,9 +1,10 @@
 // Frame drawn in the last 1s
 let frames: number[] = [];
 
+const second = 3;
 const tick = (now: DOMHighResTimeStamp) => {
   frames.push(now);
-  frames = frames.filter((e) => e > now - 1000);
+  frames = frames.filter((e) => e > now - second * 1000);
   requestAnimationFrame(tick);
 };
 tick(performance.now());
@@ -11,5 +12,6 @@ tick(performance.now());
 export function getFPS() {
   const firstFrame = frames[0] || 0;
   const lastFrame = frames[frames.length - 1] || 0;
-  return frames.length + Math.round((1000 - lastFrame + firstFrame) / (1000 / 60));
+  const t = frames.length >= 2 ? (lastFrame - firstFrame) / frames.length : 1000 / 60;
+  return Math.round((frames.length + (second * 1000 - (lastFrame - firstFrame)) / t) / second);
 }
