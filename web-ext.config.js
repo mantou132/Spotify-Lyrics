ddef780ba7134cde17fa0903f901af66897fdb4f
mantou132 <709922234@qq.com>
2020-10-23 13:26:32 +0800

    Add release CI

--- web-ext.config.js
+++ web-ext.config.js
@@ -1,17 +1,7 @@
-const api = {};
-
-try {
-  Object.assign(api, require('./apikey'));
-} catch (e) {}
-console.log(api);
 module.exports = {
   sourceDir: 'extension',
   artifactsDir: 'build',
   verbose: true,
-  sign: {
-    apiKey: api.apiKey || '',
-    apiSecret: api.apiSecret || '',
-  },
   build: {
     overwriteDest: true,
   },
