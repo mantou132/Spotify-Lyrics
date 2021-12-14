f0eace9a29bd02e6dbcf1c64ce331300fc6dc86d
wangxianqiao <wangxianqiao@cloudfine.com>
2021-12-15 01:13:21 +0800

    Fixed youtube button style

--- src/page/config.ts
+++ src/page/config.ts
@@ -79,7 +79,7 @@ export const localConfig: LocalConfig = (() => {
           margin-left: var(--ytmusic-like-button-renderer-button-spacing, 8px);
         }
         .${LYRICS_CLASSNAME} tp-yt-iron-icon {
-          background: var(--ytmusic-icon-inactive);
+          background: var(--iron-icon-fill-color, currentcolor);
           transform: rotate(90deg) scale(1.2);
           -webkit-mask: url(${iconUrl}) center / 100% no-repeat;
           mask: url(${iconUrl}) center / 100% no-repeat;
